7021e2730f3f799b39896e5c505d556068f0ba31
Lolom <lorenzoaoulini@gmail.com>
2023-12-27 19:52:32 +0100

    fix desktop width

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,7 @@ import Contact from "./ContactPage";
 
 export default function Header() {
   return (
-    <header className="mx-auto my-0 flex w-full max-w-5xl items-center justify-between px-8 py-4 laptop:mx-4 laptop:mb-8 laptop:max-w-full laptop:p-4">
+    <header className="mx-auto my-0 flex w-full max-w-5xl items-center justify-between px-8 py-4 laptop:mx-4 laptop:mb-8 laptop:w-auto laptop:max-w-full laptop:p-4">
       <div className="flex w-8 items-center gap-4 tablet:w-10">
         <Image src="/logo.png" alt="Logo" width={30} height={30} />
       </div>
